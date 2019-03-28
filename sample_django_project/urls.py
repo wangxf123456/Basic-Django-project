6c380d92e15c85711c03df8a29c5addc5af61155
Xiaofei Wang <6218006+wangxf123456@users.noreply.github.com>
2019-03-28 13:53:23 -0700

    Add index page.

--- sample_django_project/urls.py
+++ sample_django_project/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('polls.urls')),
 ]
+
+# Only serve static files from Django during development
+# Use Google Cloud Storage or an alternative CDN for production
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
